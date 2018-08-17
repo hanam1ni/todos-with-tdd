f8baae8f3ded38b264eaac160fde1a5fb0711f54
micky <tinn@nimbl3.com>
2018-08-17 15:06:14 +0700

    Refactor todo

--- app/controllers/completions_controller.rb
+++ app/controllers/completions_controller.rb
@@ -1,6 +1,13 @@
 class CompletionsController < ApplicationController
   def create
-    Todo.find(params[:todo_id]).touch :completed_at
+    todo.touch :completed_at
+
     redirect_to todos_path
+  end
+
+  private
+
+  def todo
+    Todo.find(params[:todo_id])
   end
 end
